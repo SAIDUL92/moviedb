858f2965a30e1b86707e8404c68efe9f57c51d85
MOHAMMAD SAIDUL ISLAM <isaidul92@gmail.com>
2024-04-17 17:54:38 +0600

    added  language switcher
    added  language switcher

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,43 +1,62 @@
 import Image from "next/image";
 import Link from "next/link";
+import LanguageSwitcher from "../LanguageSwitcher";
 
 function Header() {
-    return (
-        <header>
-            <nav className="container flex items-center justify-between space-x-10 py-6">
-                <Link href="/">
-                    <Image src="/assets/logo.svg" width={139} height={26} alt="image" />
-                </Link>
-                <ul className="flex items-center space-x-5">
-                    <li>
-                        <Link
-                            className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
-                            href="#"
-                        >
-                            <Image src="/assets/ring.svg" width={24} height={24} alt="image" />
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
-                            href="#"
-                        >
-                            <Image src="/assets/icons/sun.svg" width={24} height={24} alt="image" />
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
-                            href="#"
-                        >
-                            <Image src="/assets/shopping-cart.svg" width={24} height={24} alt="image" />
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+  return (
+    <header>
+      <nav className="container flex items-center justify-between space-x-10 py-6">
+        <Link href="/">
+          <Image src="/assets/logo.svg" width={139} height={26} alt="image" />
+        </Link>
 
-    )
+        <div className="flex items-center space-x-10">
+          <LanguageSwitcher />
+          <ul className="flex items-center space-x-5">
+            <li>
+              <Link
+                className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+                href="#"
+              >
+                <Image
+                  src="/assets/ring.svg"
+                  width={24}
+                  height={24}
+                  alt="image"
+                />
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+                href="#"
+              >
+                <Image
+                  src="/assets/icons/sun.svg"
+                  width={24}
+                  height={24}
+                  alt="image"
+                />
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+                href="#"
+              >
+                <Image
+                  src="/assets/shopping-cart.svg"
+                  width={24}
+                  height={24}
+                  alt="image"
+                />
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
